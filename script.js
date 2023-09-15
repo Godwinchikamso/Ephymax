ec72fb2232e9dcb0db82fd1f31ae37c7fd370076
Godwinchikamso <79155703+Godwinchikamso@users.noreply.github.com>
2023-09-15 22:58:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+    showPopup(true)
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+  }
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
